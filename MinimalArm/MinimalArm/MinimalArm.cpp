dc05f8d96613f914e675ce51a489bb877ef0f55c
Mario <MaRumrich@gmx.de>
2014-12-04 16:33:16 +0100

    Arm moves to a Position

--- MinimalArm/MinimalArm/MinimalArm.cpp
+++ MinimalArm/MinimalArm/MinimalArm.cpp
@@ -3,13 +3,75 @@
 #include <iostream>           // std::cout
 #include <memory>             // std::unique_ptr
 #include "libkindrv/kindrv.h" // KinDrv::JacoArm
+#include <thread>
+
+
+KinDrv::jaco_position_t ArmMoveTo(KinDrv::JacoArm* arm, float Position[3], float Rotation[3], float Finger[3]){
+	arm->start_api_ctrl();
+
+	KinDrv::jaco_position_t Pos1 = arm->get_cart_pos();
+	Position[0] = Position[0] * 0.01;
+	Position[1] = Position[1] * 0.01;
+	Position[2] = Position[2] * 0.01;
+	Pos1.position[0] = Position[0];
+	Pos1.position[1] = Position[1];
+	Pos1.position[2] = Position[2];
+
+	Pos1.rotation[0] = Position[0];
+	Pos1.rotation[1] = Position[1];
+	Pos1.rotation[2] = Position[2];
+
+	Pos1.finger_position[0] = Position[0];
+	Pos1.finger_position[1] = Position[1];
+	Pos1.finger_position[2] = Position[2];
+
+	KinDrv::jaco_position_t PosNow = arm->get_cart_pos();
+	float Diff_Position[3] = { 5, 5, 5 };
+	float Diff_Rotation[3] = { 5, 5, 5 };
+	float Diff_Finger[3] = { 5, 5, 5 };
+
+	arm->set_target_cart(Position[0], Position[1], Position[2], Rotation[0], Rotation[1], Rotation[2], Finger[0], Finger[1], Finger[2]);
+
+	while (Diff_Position[0] > 0.0005 ||
+		Diff_Position[1] > 0.0005 ||
+		Diff_Position[2] > 0.0005 ||
+		Diff_Rotation[0] > 0.0005 ||
+		Diff_Rotation[1] > 0.0005 ||
+		Diff_Rotation[2] > 0.0005 ||
+		Diff_Finger[0] > 0.0005 ||
+		Diff_Finger[1] > 0.0005 ||
+		Diff_Finger[2] > 0.0005
+		)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		PosNow = arm->get_cart_pos();
+		Diff_Position[0] = sqrt(pow(Position[0] - PosNow.position[0], 2));
+		Diff_Position[1] = sqrt(pow(Position[1] - PosNow.position[1], 2));
+		Diff_Position[2] = sqrt(pow(Position[2] - PosNow.position[2], 2));
+		Diff_Rotation[0] = sqrt(pow(Rotation[0] - PosNow.rotation[0], 2));
+		Diff_Rotation[1] = sqrt(pow(Rotation[1] - PosNow.rotation[1], 2));
+		Diff_Rotation[2] = sqrt(pow(Rotation[2] - PosNow.rotation[2], 2));
+		Diff_Finger[0] = sqrt(pow(Finger[0] - PosNow.finger_position[0], 2));
+		Diff_Finger[1] = sqrt(pow(Finger[1] - PosNow.finger_position[1], 2));
+		Diff_Finger[2] = sqrt(pow(Finger[2] - PosNow.finger_position[2], 2));
+
+		arm->set_target_cart(Position[0], Position[1], Position[2], Rotation[0], Rotation[1], Rotation[2], Finger[0], Finger[1], Finger[2]);
+
+		//std::cin.ignore();
+	}
+	Pos1 = arm->get_cart_pos();
+	arm->stop_api_ctrl();
+	
+	return Pos1;
+}
 
 int main(int argc, char* argv[])
 {
 	KinDrv::init_usb();
-	std::unique_ptr<KinDrv::JacoArm> arm;
-/*	try{
-		arm = std::unique_ptr<KinDrv::JacoArm>(new KinDrv::JacoArm());		
+	KinDrv::JacoArm* arm;
+
+	try{
+		arm = new KinDrv::JacoArm();		
 	}
 	catch (KinDrv::KinDrvException e){
 		std::cout << e.what() << std::endl;
@@ -18,15 +80,25 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 	std::cout << "Arm found!" << std::endl;
-	
 	KinDrv::jaco_position_t Pos1 = arm->get_cart_pos();
+	
+	printf("position: %f, %f, %f \n", Pos1.position[0], Pos1.position[1], Pos1.position[2]);
+	printf("rotation: %f, %f, %f \n", Pos1.rotation[0], Pos1.rotation[1], Pos1.rotation[2]);
+	printf("Finger:   %f, %f, %f \n", Pos1.finger_position[0], Pos1.finger_position[1], Pos1.finger_position[2]);
 
-	printf("position: %f , %f, %f", Pos1.position[0], Pos1.position[1], Pos1.position[2]);
 	std::cin.ignore();
+	float Position[3] = { 0, -0.50, 0.50 };
+	float Rotation[3] = { 3, 0, -3 };
+	float Finger[3] = { 10, 10, 10 };
+	
+	arm->set_target_cart(Position[0], Position[1], Position[2], Rotation[0], Rotation[1], Rotation[2], Finger[0], Finger[1], Finger[2]);
+	
+//	Pos1 = ArmMoveTo(arm, Position, Rotation, Finger);
+	Pos1 = arm->get_cart_pos();
+	printf("position: %f, %f, %f \n", Pos1.position[0], Pos1.position[1], Pos1.position[2]);
+	printf("rotation: %f, %f, %f \n", Pos1.rotation[0], Pos1.rotation[1], Pos1.rotation[2]);
+	printf("Finger:   %f, %f, %f \n", Pos1.finger_position[0], Pos1.finger_position[1], Pos1.finger_position[2]);
 
-	arm->start_api_ctrl();
-	arm->set_target_cart(Pos1.position, Pos1.finger_position);
-
-	std::cin.ignore(); */
+	std::cin.ignore(); 
 	return 0;
 }
