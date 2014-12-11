9c2202a24b93557574e5c5af1d623cfb080a2ab5
BenjaminW3 <benjaminworpitz@gmail.com>
2014-12-11 13:04:00 +0100

    Catch all exceptions in main and print them

--- Kinjo/Kinjo/src/Kinjo.cpp
+++ Kinjo/Kinjo/src/Kinjo.cpp
@@ -1,8 +1,23 @@
 #include <kinjo/arm/ArmFactory.hpp>
 
+#include <opencv2/highgui/highgui.hpp>
+
 int main(int argc, char* argv[]){
 
-	auto const Arm = kinjo::arm::ArmFactory::getInstance(1);
+	try
+	{
+		std::shared_ptr<kinjo::arm::Arm> const Arm = kinjo::arm::ArmFactory::getInstance(1);
 
-	return 0;
+		return 0;
+    }
+    catch(std::exception const & e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    catch(...)
+    {
+        std::cerr << "Unknown exception!" << std::endl;
+        return 1;
+    }
 }
