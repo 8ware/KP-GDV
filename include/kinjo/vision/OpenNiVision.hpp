bb92484a1f20c32181a9810d4a8b39d3a41e53fa
8ware <8wared@googlemail.com>
2014-12-17 18:48:37 +0100

    Transferred vision-prototype code to actual implementation
    * derived OpenNiVision from prototype
    * adjusted header files
    * added test and makefile

--- include/kinjo/vision/OpenNiVision.hpp
+++ include/kinjo/vision/OpenNiVision.hpp
@@ -2,6 +2,12 @@
 
 #include <kinjo/vision/Vision.hpp>
 
+#include <string>
+
+// OpenNI
+#include <XnCppWrapper.h>
+
+
 namespace kinjo {
     namespace vision {
 
@@ -9,12 +15,60 @@ namespace kinjo {
         {
 
         public:
-            /**
-             * \return the depth values normalized to centimetres.
-             */
-            virtual cv::Mat getDepth() const override;
-            virtual cv::Mat getRgb() const override;
+			/**
+			 * The default constructor.
+			 */
+			OpenNiVision();
 
+			/**
+			 * \return the depth values normalized to centimeters.
+			 */
+			virtual cv::Mat getDepth();
+			/**
+			 * \return the RGB image already translated to OpenCV's internal
+			 * BGR representation.
+			 */
+			// TODO maybe rename to getBgr?
+			virtual cv::Mat getRgb();
+
+		private:
+			/**
+			 * The status which is set by OpenNI.
+			 */
+			XnStatus status;
+			/**
+			 * The context which keeps the depth- and RGB-production nodes.
+			 */
+			xn::Context context;
+			/**
+			 * The actual depth image generator (production node).
+			 */
+			xn::DepthGenerator depthGenerator;
+			/**
+			 * The size of the depth-generator's images.
+			 */
+			cv::Size depthImageSize;
+			/**
+			 * The actual RGB image generator (production node).
+			 */
+			xn::ImageGenerator rgbGenerator;
+			/**
+			 * The size of the RGB-generator's images.
+			 */
+			cv::Size rgbImageSize;
+
+			/**
+			 * Helper function to check the OpenNI status.
+			 * \param action a message describing the action during which the
+			 * error occurred
+			 */
+			void checkStatus(std::string action) const;
+			/**
+			 * Helper function to get the generator's output image size.
+			 * \param generator the generator which produces image maps
+			 * \return the (OpenCV) size of the generated image.
+			 */
+			cv::Size getImageSize(xn::MapGenerator& generator) const;
         };
 
     }
---
+++ test/OpenNiVisionTest.cpp
@@ -0,0 +1,81 @@
+
+#include <kinjo/vision/Vision.hpp>
+#include <kinjo/vision/OpenNiVision.hpp>
+
+#include <sstream>
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+using namespace std;
+using namespace cv;
+using namespace kinjo::vision;
+
+
+#define TITLE_DEPTH "Vision (Depth)"
+#define TITLE_RGB "Vision (RGB)"
+#define REFRESH_INTERVAL 100
+
+
+void updateCoordinates(int event, int x, int y, int flags, void *param) {
+	Point* point = static_cast<Point*>(param);
+
+	switch (event) {
+		case CV_EVENT_LBUTTONUP:
+			*point = Point(x, y);
+			break;
+	}
+}
+
+void displayCoordinates(Mat& image, Point& point, Scalar& color) {
+	circle(image, point, 3, color);
+	circle(image, point, 7, color);
+}
+
+void displayDistance(Mat& image, Point& point, Scalar& color, ushort distance) {
+	float dist_cm = distance / 10;
+	stringstream stream;
+	stream << "[ " << dist_cm << "cm ]";
+	Point shifted = point + Point(15, -5);
+	putText(image, stream.str(), shifted, FONT_HERSHEY_SIMPLEX, 0.3, color);
+}
+
+int main(void) {
+	Vision* vision;
+	vision = new OpenNiVision();
+
+	Point point;
+	namedWindow(TITLE_DEPTH);
+	namedWindow(TITLE_RGB);
+	setMouseCallback(TITLE_DEPTH, updateCoordinates, &point);
+	setMouseCallback(TITLE_RGB, updateCoordinates, &point);
+
+	Scalar depthColor = Scalar(255 << 8);
+	Scalar rgbColor = Scalar(0, 255, 0);
+
+	Mat depth, rgb;
+	do {
+		depth = vision->getDepth();
+		rgb = vision->getRgb();
+
+		if (0 <= point.x && point.x < depth.cols
+				&& 0 <= point.y && point.y < depth.rows) {
+			displayCoordinates(depth, point, depthColor);
+			displayCoordinates(rgb, point, rgbColor);
+
+			ushort distance = depth.at<ushort>(point.y, point.x);
+			if (distance > 0) {
+				displayDistance(depth, point, depthColor, distance);
+				displayDistance(rgb, point, rgbColor, distance);
+			}
+
+		}
+
+		imshow(TITLE_DEPTH, depth * 10);
+		imshow(TITLE_RGB, rgb);
+	} while (waitKey(REFRESH_INTERVAL) != 27);
+
+	return 0;
+}
+
