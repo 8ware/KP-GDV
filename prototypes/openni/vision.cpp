46c8c89f5009b25ab4efd91136e82fc3625e48c5
8ware <8wared@googlemail.com>
2014-12-17 14:24:27 +0100

    Improved openni prototype
    * added RGB image tapping
    * added more efficient conversion of OpenNI's pixel-pointer to OpenCV's
      matrix
    * added click-handler to draw circles and show distance

--- prototypes/openni/vision.cpp
+++ prototypes/openni/vision.cpp
@@ -2,6 +2,7 @@
 // standard libraries
 #include <iostream>
 #include <string>
+#include <sstream>
 
 // OpenNI
 #include <XnCppWrapper.h>
@@ -21,6 +22,8 @@ using namespace xn;
 
 
 #define TITLE "Vision"
+#define TITLE_DEPTH "Vision (DEPTH)"
+#define TITLE_RGB "Vision (RGB)"
 /*
  * TODO get height and width from metadata:
  *
@@ -36,6 +39,7 @@ using namespace xn;
 
 void check_status(XnStatus& status, string what);
 void translate(const XnDepthPixel* depth_pixels, Mat& depth_map);
+void translate(const XnRGB24Pixel* rgb_pixels, Mat& rgb_map, Size size);
 void update_coordinates(int event, int x, int y, int flags, void *param);
 
 
@@ -43,9 +47,21 @@ XnStatus status;
 Context context;
 
 DepthGenerator depth_generator;
+ImageGenerator rgb_generator;
 
-Point coords = Point(-1, -1);
+Point point = Point(-1, -1);
 
+// http://denislantsman.com/?p=84
+void convert_pixel_map(const XnRGB24Pixel* pImageMap, Mat& cv_image, int rows, int cols) {
+	int sizes[2] = {rows, cols};
+	cv_image = Mat(2, sizes, CV_8UC3, (void*) pImageMap);
+}
+
+Size getOutputSize(MapGenerator& generator) {
+	XnMapOutputMode output;
+	generator.GetMapOutputMode(output);
+	return Size(output.nXRes, output.nYRes);
+}
 
 int main(void) {
 	status = context.Init();
@@ -54,38 +70,82 @@ int main(void) {
 	status = depth_generator.Create(context);
 	check_status(status, "Creating depth generator");
 
+	status = rgb_generator.Create(context);
+	check_status(status, "Creating RGB generator");
+
 	status = context.StartGeneratingAll();
 	check_status(status, "Starting generation");
 
-	namedWindow(TITLE);
-	setMouseCallback(TITLE, update_coordinates);
+	Size depthSize = getOutputSize(depth_generator);
+	Size rgbSize = getOutputSize(rgb_generator);
 
-	Mat depth_mat(ROWS, COLS, CV_16UC1);
+	namedWindow(TITLE_DEPTH);
+	namedWindow(TITLE_RGB);
+	setMouseCallback(TITLE_DEPTH, update_coordinates);
+	setMouseCallback(TITLE_RGB, update_coordinates);
+
+	Mat depth_mat;
+	Mat rgb_mat;
 	do {
-		status = context.WaitOneUpdateAll(depth_generator);
-		check_status(status, "Updating depth data");
+		status = context.WaitAnyUpdateAll(); // And/Any/One/None
+		check_status(status, "Updating depth/RGB data");
 
 		const XnDepthPixel* depth_map = depth_generator.GetDepthMap();
-		translate(depth_map, depth_mat);
+		depth_mat = Mat(depthSize, CV_16UC1, (void*) depth_map);
 
-		if (coords.x >= 0 && coords.y >= 0) {
-			ushort distance = depth_mat.at<ushort>(coords.y, coords.x);
-			cout << "Distance: " << distance << "mm" << endl;
-			coords = Point(-1, -1);
+		const XnRGB24Pixel* rgb_map = rgb_generator.GetRGB24ImageMap();
+		rgb_mat = Mat(rgbSize, CV_8UC3, (void*) rgb_map);
+//		translate(rgb_map, rgb_mat, rgbSize);
+
+		Mat depth, rgb;
+		depth_mat.copyTo(depth);
+		rgb_mat.copyTo(rgb);
+
+		if (0 <= point.x && point.x < depth_mat.cols
+				&& 0 <= point.y && point.y < depth_mat.rows) {
+			ushort distance = depth_mat.at<ushort>(point.y, point.x);
+//			cout << "Distance: " << distance << "mm" << endl;
+//			Scalar color = rgb_mat.at<Scalar>(point.y, point.x);
+//			int color = rgb_mat.at<uchar>(point.y, point.x);
+//			cout << "Color:    " << color << endl;
+
+			Scalar d_color = Scalar(255 << 8);
+			circle(depth, point, 3, d_color);
+			circle(depth, point, 7, d_color);
+
+			Scalar color = Scalar(0, 255, 0);
+			circle(rgb, point, 3, color);
+			circle(rgb, point, 7, color);
+
+			if (distance > 0) {
+				stringstream stream;
+				stream << "[ " << distance << "mm ]";
+				putText(depth, stream.str(), point + Point(15, -5), FONT_HERSHEY_SIMPLEX, 0.3, d_color);
+				putText(rgb, stream.str(), point + Point(15, -5), FONT_HERSHEY_SIMPLEX, 0.3, color);
+			}
+//			point = Point(-1, -1);
 		}
 
-		imshow(TITLE, depth_mat);
+		imshow(TITLE_DEPTH, depth);
+		imshow(TITLE_RGB, rgb);
+
+		depth_mat.release();
+		rgb_mat.release();
 	} while (waitKey(REFRESH_INTERVAL) != 27);
 
 	return 0;
 }
 
 
-void translate(const XnDepthPixel* depth_pixels, Mat& depth_map) {
-	for (int y = 0; y < ROWS; y++) {
-		for (int x = 0; x < COLS; x++, depth_pixels++) {
-			XnDepthPixel depth = *depth_pixels;
-			depth_map.at<ushort>(y, x) = depth;
+void translate(const XnRGB24Pixel* rgb_pixels, Mat& rgb_map, Size size) {
+	rgb_map = Mat(size, CV_8UC3);
+	cout << "RGB=" << rgb_map.size() << endl;
+	for (int y = 0; y < size.height; y++) {
+		for (int x = 0; x < size.width; x++) {
+			XnRGB24Pixel rgb = *rgb_pixels;
+			Scalar bgr = Scalar(rgb.nBlue, rgb.nGreen, rgb.nRed);
+			rgb_map.at<Scalar>(y, x) = bgr;
+			rgb_pixels++;
 		}
 	}
 }
@@ -100,7 +160,8 @@ void check_status(XnStatus& status, string what) {
 void update_coordinates(int event, int x, int y, int flags, void *param) {
 	switch (event) {
 		case CV_EVENT_LBUTTONUP:
-			coords = Point(x, y);
+			point = Point(x, y);
+//			cout << point << endl;
 			break;
 	}
 }
