b63106e52a43d44c9c7668397df456548a573df1
Akrabor <henrik.schole@gmx.de>
2014-12-17 14:12:42 +0100

    fixed kinjo gui

--- src/Kinjo.cpp
+++ src/Kinjo.cpp
@@ -4,7 +4,7 @@
 //#include <cstdio>
 #include <iostream>
 
-const int slider_max = 100;
+const int slider_max = 50;
 
 //not yet needed::eventHandler for slider(s)
 void on_trackbar(int, void*){}
@@ -17,12 +17,21 @@ void jacoMove(std::shared_ptr<kinjo::arm::Arm> Arm){
 		cv::getTrackbarPos("Y", "kinjo"),
 		cv::getTrackbarPos("Z", "kinjo")
 	);
-	Arm->moveTo(vector);
-	std::printf("moving to %i,%i,%i is done done.\n", 
+	std::printf("moving to %i,%i,%i ...\n",
 		cv::getTrackbarPos("X", "kinjo"),
 		cv::getTrackbarPos("Y", "kinjo"),
 		cv::getTrackbarPos("Z", "kinjo")
-	);
+		);
+	if (Arm != nullptr){
+		Arm->moveTo(vector);
+
+	}
+	
+	std::printf("moving done. New \"exact\" Position: %f,%f,%f\n",
+		Arm->getPosition()[0], 
+		Arm->getPosition()[1], 
+		Arm->getPosition()[2]
+		);
 
 }
 
@@ -30,16 +39,19 @@ void jacoMove(std::shared_ptr<kinjo::arm::Arm> Arm){
 
 int main(int argc, char* argv[]){
 	
+	int posX;
+	int posY;
+	int posZ;
+
+
 	cv::namedWindow("kinjo", cv::WINDOW_AUTOSIZE);
 	
-	cv::createTrackbar("X", "kinjo", 0, slider_max, on_trackbar);
-	cv::createTrackbar("Y", "kinjo", 0, slider_max, on_trackbar);
-	cv::createTrackbar("Z", "kinjo", 0, slider_max, on_trackbar);
 	std::shared_ptr<kinjo::arm::Arm> Arm;
-	
+
 	try
 	{
 		Arm = kinjo::arm::ArmFactory::getInstance();	
+		
 		//return 0;
     }
     catch(std::exception const & e)
@@ -52,8 +64,15 @@ int main(int argc, char* argv[]){
         std::cerr << "Unknown exception!" << std::endl;
 		//return 1;
     }
+
+	posX = (int)Arm->getPosition()[0];
+	posY = (int)Arm->getPosition()[1];
+	posZ = (int)Arm->getPosition()[2];
 	
-	
+	cv::createTrackbar("X", "kinjo", &posX, slider_max, on_trackbar);
+	cv::createTrackbar("Y", "kinjo", &posY, slider_max, on_trackbar);
+	cv::createTrackbar("Z", "kinjo", &posZ, slider_max, on_trackbar);
+
 	//press esc to close window
 	//press space to move the jacoArm
 	for (;;){
