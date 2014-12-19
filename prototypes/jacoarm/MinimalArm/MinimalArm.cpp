d005ce50ed4e3a24c424fce5be8825758360fd15
sentinel89 <wildestephan@gmx.de>
2014-12-19 12:21:07 +0100

    get arm inaccuracy test

--- prototypes/jacoarm/MinimalArm/MinimalArm.cpp
+++ prototypes/jacoarm/MinimalArm/MinimalArm.cpp
@@ -8,9 +8,9 @@
 
 KinDrv::jaco_position_t ArmMoveTo(KinDrv::JacoArm* arm, float Position[3], float Rotation[3], float Finger[3]){
 	KinDrv::jaco_position_t Pos1 = arm->get_cart_pos();
-	Position[0] = Position[0] * 0.01;
-	Position[1] = Position[1] * 0.01;
-	Position[2] = Position[2] * 0.01;
+	Position[0] = Position[0] * 0.01f;
+	Position[1] = Position[1] * 0.01f;
+	Position[2] = Position[2] * 0.01f;
 	Pos1.position[0] = Position[0];
 	Pos1.position[1] = Position[1];
 	Pos1.position[2] = Position[2];
@@ -63,6 +63,50 @@ KinDrv::jaco_position_t ArmMoveTo(KinDrv::JacoArm* arm, float Position[3], float
 	return Pos1;
 }
 
+
+KinDrv::jaco_position_t ArmMoveTo(KinDrv::JacoArm* arm, float Position[3]){
+	KinDrv::jaco_position_t Pos1 = arm->get_cart_pos();
+
+	Position[0] = Position[0];// * 0.01f;
+	Position[1] = Position[1];// * 0.01f;
+	Position[2] = Position[2];// *0.01f;
+	Pos1.position[0] = Position[0];
+	Pos1.position[1] = Position[1];
+	Pos1.position[2] = Position[2];
+
+	KinDrv::jaco_position_t PosNow = arm->get_cart_pos();
+	float Diff_Position[3] = { 5, 5, 5 };
+
+	arm->set_target_cart(Position[0], Position[1], Position[2], Pos1.rotation[0], Pos1.rotation[1], Pos1.rotation[2], Pos1.finger_position[0], Pos1.finger_position[1], Pos1.finger_position[2]);
+
+	while (Diff_Position[0] > 0.01 ||
+		Diff_Position[1] > 0.01 ||
+		Diff_Position[2] > 0.01
+		)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		PosNow = arm->get_cart_pos();
+		Diff_Position[0] = sqrt(pow(Position[0] - PosNow.position[0], 2));
+		Diff_Position[1] = sqrt(pow(Position[1] - PosNow.position[1], 2));
+		Diff_Position[2] = sqrt(pow(Position[2] - PosNow.position[2], 2));
+
+		arm->set_target_cart(Position[0], Position[1], Position[2], Pos1.rotation[0], Pos1.rotation[1], Pos1.rotation[2], Pos1.finger_position[0], Pos1.finger_position[1], Pos1.finger_position[2]);
+
+		//std::cin.ignore();
+	}
+	Pos1 = arm->get_cart_pos();
+	arm->stop_api_ctrl();
+
+	return Pos1;
+}
+
+
+void Moveandprint(KinDrv::JacoArm* arm,float m_x, float m_y, float m_z){
+	float p[3] = {m_x,m_y,m_z};
+	KinDrv::jaco_position_t nachher = ArmMoveTo(arm, p);
+	printf("nachher: x:%f y:%f z:%f \nSoll: x:%f y:%f z:%f", nachher.position[0], nachher.position[1], nachher.position[2],m_x,m_y,m_z );
+}
+
 int main(int argc, char* argv[])
 {
 	KinDrv::init_usb();
@@ -84,19 +128,28 @@ int main(int argc, char* argv[])
 	printf("rotation: %f, %f, %f \n", Pos1.rotation[0], Pos1.rotation[1], Pos1.rotation[2]);
 	printf("Finger:   %f, %f, %f \n", Pos1.finger_position[0], Pos1.finger_position[1], Pos1.finger_position[2]);
 
+	//Test
+	float delta = 0.15f;//meter!
+	for (int i = 1; i < 3; i++){
+		KinDrv::jaco_position_t p = arm->get_cart_pos();
+		p.position[0] += delta * ((i % 2) ? 1 : -1);
+		Moveandprint(arm, p.position[0], p.position[1], p.position[2]);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+
 	std::cin.ignore();
-	float Position[3] = { 0, -0.50, 0.50 };
+	/*float Position[3] = { 0, -0.50, 0.50 };
 	float Rotation[3] = { 3, 0, -3 };
-	float Finger[3] = { 10, 10, 10 };
+	float Finger[3] = { 10, 10, 10 };*/
 	
-	arm->set_target_cart(Position[0], Position[1], Position[2], Rotation[0], Rotation[1], Rotation[2], Finger[0], Finger[1], Finger[2]);
+	//arm->set_target_cart(Position[0], Position[1], Position[2], Rotation[0], Rotation[1], Rotation[2], Finger[0], Finger[1], Finger[2]);
 	
 //	Pos1 = ArmMoveTo(arm, Position, Rotation, Finger);
-	Pos1 = arm->get_cart_pos();
+	/*Pos1 = arm->get_cart_pos();
 	printf("position: %f, %f, %f \n", Pos1.position[0], Pos1.position[1], Pos1.position[2]);
 	printf("rotation: %f, %f, %f \n", Pos1.rotation[0], Pos1.rotation[1], Pos1.rotation[2]);
 	printf("Finger:   %f, %f, %f \n", Pos1.finger_position[0], Pos1.finger_position[1], Pos1.finger_position[2]);
-
+	*/
 	std::cin.ignore(); 
 	return 0;
 }
