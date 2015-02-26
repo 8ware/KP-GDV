1faf39989f717fc9e4360fa89285fe70904036fd
Akrabor <henrik.schole@gmx.de>
2015-02-26 15:41:33 +0100

    added mockarm
    not yet included into project

---
+++ include/kinjo/arm/MockArm.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <kinjo/arm/Arm.hpp>
+
+#include <memory>
+
+// forward declarations
+namespace KinDrv {
+	class MockArm;
+}
+
+namespace kinjo {
+	namespace arm {
+
+		class MockArm final : public Arm
+		{
+
+		public:
+			MockArm();
+			
+			/**
+			* \param vector the absolute position in centimetres.
+			*/
+			void moveTo(cv::Vec3f vector) override;
+
+			/**
+			* \param vector the absolute grab rotation in Degree
+			*/
+			void rotateTo(cv::Vec3f vector) override;
+
+			/**
+			* \param vector the relative position in centimetres.
+			*/
+			void moveBy(cv::Vec3f vector) override;
+
+			/**
+			* \param vector the relative Rotation in Degree.
+			*/
+			void rotateBy(cv::Vec3f vector) override;
+
+			/**
+			* \return the absolute position in centimetres.
+			*/
+			cv::Vec3f getPosition() const override;
+			/**
+			* \return the absolute rotation in degree
+			**/
+			cv::Vec3f getRotation() const override;
+			
+
+
+			void openFingers() override;
+			void closeFingers() override;
+
+		private:
+			/**
+			* Helper to wait for the arm finish moving.
+			**/
+			void waitArmFinishMovement() const;
+			/**
+			* Helper to find out if the arm is moving.
+			* \return True if the arm is moving (or tries to move).
+			**/
+			bool isArmMoving() const;
+
+			/**
+			* Helper to get a bool if 2 float values are the same
+			* \return True if both values are the same
+			**/
+			bool DiffIsZero(float X, float Y) const;
+
+		private:
+			
+			//state representation of arm
+			mutable cv::Vec3f cart_pos;
+			mutable cv::Vec3f cart_rot;
+			mutable cv::Vec3f cart_fin;
+
+			//accuracy reduction
+			mutable float pos_offset = 0;
+			mutable float rot_offset = 0;
+			mutable float fin_offset = 0;
+		
+		};//class
+
+	}
+} //namespace
---
+++ src/arm/MockArm.cpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <kinjo/arm/MockArm.hpp>
+
+//#include <libkindrv/kindrv.h>   //// KinDrv::MockArm
+
+#include <thread>
+#include <iostream>
+
+namespace kinjo {
+	namespace arm {
+
+		static const float pi = 3.14159265358979323846f;
+
+		MockArm::MockArm()
+		{
+			//any start values
+			pos_offset = 0.1;
+			rot_offset = 0.1;
+			fin_offset = 10;
+			cart_pos = cv::Vec3f(0.050, 0.050, 0.050);
+			cart_rot = cv::Vec3f(1, 1, 1);
+			cart_fin = cv::Vec3f(10, 10, 10); 
+			
+			std::cout << "MockArm found, using Mock Arm." << std::endl;
+		}
+
+		void MockArm::moveTo(cv::Vec3f vector)
+		{
+			//works with milimeter, accuracy is bad though
+			cart_pos[0] = vector[0] / 1000 + pos_offset;
+			cart_pos[1] = vector[1] / 1000 + pos_offset;
+			cart_pos[2] = vector[2] / 1000 + pos_offset;
+			waitArmFinishMovement();
+		}
+		void MockArm::rotateTo(cv::Vec3f vector)
+		{
+			cart_rot[0] = vector[0] / 1000 + rot_offset;
+			cart_rot[1] = vector[1] / 1000 + rot_offset;
+			cart_rot[2] = vector[2] / 1000 + rot_offset;
+			waitArmFinishMovement();
+		}
+		void MockArm::moveBy(cv::Vec3f vector)
+		{
+			cart_pos[0] += vector[0] / 1000 + pos_offset;
+			cart_pos[1] += vector[1] / 1000 + pos_offset;
+			cart_pos[2] += vector[2] / 1000 + pos_offset;
+			waitArmFinishMovement();
+		}
+		void MockArm::rotateBy(cv::Vec3f vector)
+		{
+			// TODO: Test
+			cart_rot[0] += vector[0] / 1000 + rot_offset;
+			cart_rot[1] += vector[1] / 1000 + rot_offset;
+			cart_rot[2] += vector[2] / 1000 + rot_offset;
+
+			waitArmFinishMovement();
+		}
+
+		void MockArm::openFingers()
+		{
+			//TODO: test if the numbers are right
+			cart_fin[0] = 55 + fin_offset;
+			cart_fin[1] = 55 + fin_offset;
+			cart_fin[2] = 55 + fin_offset;
+			waitArmFinishMovement();
+		}
+		void MockArm::closeFingers()
+		{
+			//TODO: test if the numbers are right
+			cart_fin[0] = 0 + fin_offset;
+			cart_fin[1] = 0 + fin_offset;
+			cart_fin[2] = 0 + fin_offset;
+			waitArmFinishMovement();
+		}
+
+		cv::Vec3f MockArm::getRotation() const
+		{
+			return cart_rot;
+		}
+
+		cv::Vec3f MockArm::getPosition() const
+		{
+			return cart_pos;
+		}
+
+		void MockArm::waitArmFinishMovement() const
+		{
+			while (isArmMoving())
+			{
+				//don't fire too many commands at the arm
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
+			}
+		}
+
+		bool MockArm::isArmMoving() const
+		{
+			return false;
+		}
+
+		bool MockArm::DiffIsZero(float X, float Y) const{
+			return (static_cast<int> (X * 1000) == static_cast<int>(Y * 1000));
+		}
+	}
+}
